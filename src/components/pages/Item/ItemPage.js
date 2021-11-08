68cf1f497be9aebf8b7b3b7f437ebaedb1ddc950
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-11-08 20:13:16 -0300

    start feat item

---
+++ src/components/pages/Item/ItemPage.js
@@ -0,0 +1,25 @@
+import {
+    ItemPageContainer,
+    ItemContainer,
+} from './ItemPageStyle'
+
+export default function ItemPage() {
+    return (
+        <ItemPageContainer>
+            <ItemContainer>
+                <div className='left-side'>
+
+                </div>
+
+                <div className='right-side'>
+                    <div>
+                        <span>moças /</span>
+                        <span>roupas /</span>
+                        <span>vestidos /</span>
+                    </div>
+                    <title>vestido midi ciganinha verde bandeira com fenda</title>
+                </div>
+            </ItemContainer>
+        </ItemPageContainer>
+    )
+}
---
+++ src/components/pages/Item/ItemPageStyle.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const ItemPageContainer = styled.main`
+    margin-top: 100px;
+    padding: 20px 40px;
+`
+
+const ItemContainer = styled.section`
+
+`
+
+export {
+    ItemPageContainer,
+    ItemContainer,
+}
